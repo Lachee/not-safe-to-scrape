23a2c9dbc323f4af7d2f8af0d10ec6aa6548a8e9
Lachee <lachlan.h@hotmail.com>
2020-12-07 20:36:29 +1100

    fixed generic

--- src/scraper/generic.js
+++ src/scraper/generic.js
@@ -24,57 +24,56 @@ const levenshteinFilter = (source, maximum = 5) => {
   }
 
 module.exports = async function(url) {
+
+    // Get the metadata
+    let scrapeResult = {
+        id:             md5(url),
+        type:           'artwork',
+        title:          null,
+        description:    null,
+        artist:         null,
+        tags:           [],
+        url:            url,
+        images:         [],
+        thumbnail:      null,
+    };
+
+    const response = await fetch(url, { method: 'GET' });
+    const page =  await response.text();
+    const $ = cheerio.load(page);
+
+    let pageImages = [];
+    $('img').each((i, element) => {
+        pageImages.push($(element).attr('src') || $(element).attr('srcset') || $(element).attr('data-src'));
+    });
+
+    console.log('all images', pageImages);
+    scrapeResult.title = $("meta[property='og:title']").attr("content");
+    scrapeResult.description =  $("meta[property='og:description']").attr("content");
+
+    const levenshteined = levenshteinFilter(pageImages, 10);
+    const images = levenshteined.filter(g => g.length > 3).flat();
+
+    scrapeResult.images = images
+                            .map(url => url.startsWith('//') ? 'https:' + url : url)
+                            .filter(url => { 
+                                const s = url.split('?')[0];
+                                console.log(s);
+                                return s.endsWith('.jpg') || s.endsWith('.jpeg');
+                            });
+
+    console.log('actual images', scrapeResult.images);
     
-        // Get the metadata
-        let scrapeResult = {
-            id:             md5(url),
-            type:           'artwork',
-            title:          null,
-            description:    null,
-            artist:         null,
-            tags:           [],
-            url:            url,
-            images:         [],
-            thumbnail:      null,
-        };
+    if (scrapeResult.images.length > 0) 
+        scrapeResult.thumbnail = scrapeResult.images[0];
 
-        const response = await fetch(url, { method: 'GET' });
-        const page =  await response.text();
-        const $ = cheerio.load(page);
+    if (scrapeResult.images.length > 1) 
+        scrapeResult.type = 'comic';
 
-        let pageImages = [];
-        $('img').each((i, element) => {
-            pageImages.push($(element).attr('src') || $(element).attr('srcset') || $(element).attr('data-src'));
-        });
+    return scrapeResult;
+}
 
-        console.log('all images', pageImages);
-        scrapeResult.title = $("meta[property='og:title']").attr("content");
-        scrapeResult.description =  $("meta[property='og:description']").attr("content");
-
-        const levenshteined = levenshteinFilter(pageImages, 10);
-        const images = levenshteined.filter(g => g.length > 3).flat();
-
-        scrapeResult.images = images
-                                .map(url => url.startsWith('//') ? 'https:' + url : url)
-                                .filter(url => { 
-                                    const s = url.split('?')[0];
-                                    console.log(s);
-                                    return s.endsWith('.jpg') || s.endsWith('.jpeg');
-                                });
-
-        console.log('actual images', scrapeResult.images);
-        
-        if (scrapeResult.images.length > 0) 
-            scrapeResult.thumbnail = scrapeResult.images[0];
-
-        if (scrapeResult.images.length > 1) 
-            scrapeResult.type = 'comic';
-
-        return scrapeResult;
-    }
-
-    hasSomeParentTheClass($, element, className) {
-        if ($(element).hasClass(className)) return true;
-        return element.parentNode && this.hasSomeParentTheClass($, element.parentNode, className);
-    }
+hasSomeParentTheClass($, element, className) {
+    if ($(element).hasClass(className)) return true;
+    return element.parentNode && this.hasSomeParentTheClass($, element.parentNode, className);
 }
