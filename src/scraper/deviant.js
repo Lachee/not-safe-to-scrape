02dc35f94aec8fbd7498f3585f7e6b949b68addc
Lachee <lachlan.h@hotmail.com>
2020-12-13 00:54:49 +1100

    Temporarily removed bad deviant art stuff

--- src/scraper/deviant.js
+++ src/scraper/deviant.js
@@ -5,37 +5,66 @@ const cheerio   = require('cheerio');
 
 module.exports = async function(url) {
     
-    const response = await fetch(url, { method: 'GET' });
-    const page =  await response.text();
-    const $ = cheerio.load(page);
-
-    console.log('Recieved ', page);
-
-    const contentUrl = $('meta[property="da:appurl"]').attr('content');
-    const uuid = contentUrl.substr(contentUrl.lastIndexOf('/')+1);
+    const regex = /deviantart\.com\/([a-zA-Z0-9-]*)\//;
+    const matches = url.match(regex);
+    const username = matches[1];
+    
+    //const response = await fetch(url, { method: 'GET' });
+    //const page =  await response.text();
+    //const $ = cheerio.load(page);
+    //const contentUrl = $('meta[property="da:appurl"]').attr('content');
+    //const uuid = contentUrl.substr(contentUrl.lastIndexOf('/')+1);
     //console.log('uuid', uuid);
 
     const oauthToken = await getDeviantToken(process.env.DEVIANT_CLIENT_ID, process.env.DEVIANT_CLIENT_SECRET);
     //console.log('accessToken', oauthToken);
 
-    const deviationResponse = await fetch(`https://www.deviantart.com/api/v1/oauth2/deviation/${uuid}?access_token=${oauthToken['access_token']}`, { method: 'GET' });
-    const deviant = await deviationResponse.json();
+    let page = 0;
+    let pageLimit = 24;
+    let metadata = null;
+    while (metadata == null && page < 15) {
+        console.log('looking at', page);
+        const offset = (page++) * pageLimit;
+        const response = await fetch(`https://www.deviantart.com/api/v1/oauth2/gallery/all?username=${username}&calculate_size=false&mature_content=true&access_token=${oauthToken['access_token']}&offset=${offset}`);
+        const listing = await response.json();
+        
+        //Look for the record that has the correct url
+        const allResults = listing.results.filter(r => r.url == url)
+        if (allResults.length > 0) {
+            //metadata = allResults[0];
+            const uuid = allResults[0].deviationid;
+            const metadataResponse = await fetch(`https://www.deviantart.com/api/v1/oauth2/deviation/metadata?access_token=${oauthToken['access_token']}&deviationids%5B%5D=${uuid}&mature_content=true`, { method: 'GET' });
+            metadata = (await metadataResponse.json()).metadata[0];
+            metadata.content = allResults[0].content;
+            break;
+        }
+        
+        //Finally give up is there is no more
+        if (!listing.has_more) break;
+    }
 
-    const metadataResponse = await fetch(`https://www.deviantart.com/api/v1/oauth2/deviation/metadata?access_token=${oauthToken['access_token']}&deviationids%5B%5D=${uuid}&mature_content=true`, { method: 'GET' });
-    const metadata = (await metadataResponse.json()).metadata[0];
+    if (metadata == null) {
+        return null;
+    }
+
+    //const deviationResponse = await fetch(`https://www.deviantart.com/api/v1/oauth2/deviation/${uuid}?access_token=${oauthToken['access_token']}`, { method: 'GET' });
+    //const deviant = await deviationResponse.json();
+
+    //const metadataResponse = await fetch(`https://www.deviantart.com/api/v1/oauth2/deviation/metadata?access_token=${oauthToken['access_token']}&deviationids%5B%5D=${uuid}&mature_content=true`, { method: 'GET' });
+    //const metadata = (await metadataResponse.json()).metadata[0];
 
     const tags = metadata.tags.map(t => t.tag_name);
 
     //set the thumbnail
     return {
-        id:             uuid,
+        id:             metadata.deviationid,
         type:           tags.indexOf('comic') >= 0 ? 'comic' : 'artwork',
         title:          metadata.title,
         description:    metadata.description,
-        artist:         [ deviant.author.username ],
+        artist:         [ metadata.author.username ],
         tags:           [... new Set(tags)],
-        url:            deviant.url,
-        images:         [ deviant.content.src ],
+        url:            metadata.url,
+        images:         [ metadata.content.src ],
         cover:          null,
         pages:          1
     };
