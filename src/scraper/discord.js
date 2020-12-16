31dd13cbb6c80f3334c072216c6bf2e7ac8b5f2f
Lachee <lachlan.h@hotmail.com>
2020-12-17 02:19:37 +1100

    fixed discord

--- src/scraper/discord.js
+++ src/scraper/discord.js
@@ -4,9 +4,9 @@ const md5       = require('md5');
 //example: https://cdn.discordapp.com/attachments/741235846333595669/785374267130904617/Xdb0Fug.jpg
 
 module.exports = async function(url) {
-    const regex = /(\w*).discordapp.\w*\/attachments\/(\w*)\/(\w*)\/(\w*).((png)|(jpe?g))/;
+    const regex = /(\w*).discordapp.\w*\/attachments\/(\w*)\/(\w*)\/(.*)\.((png)|(jpe?g))/;
     const matches = url.match(regex);
-    
+    console.log(matches);
     let title   = matches[4];
     let id      = md5(matches[2] + matches[3] + matches[4]);
     let hotlink = `https://cdn.discordapp.com/attachments/${matches[2]}/${matches[3]}/${matches[4]}.${matches[5]}`;
