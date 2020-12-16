8d6712446ffe2ff469518f98e67edb132464b901
Lachee <lachlan.h@hotmail.com>
2020-12-17 02:00:25 +1100

    added better wait rules

--- src/scraper/twitter.js
+++ src/scraper/twitter.js
@@ -17,42 +17,45 @@ module.exports = async function(url) {
     //Use puppeteer to scrape the page
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    page.goto(url);
+
+    console.log('loading page...');
+    await page.goto(url);
     await page.waitForSelector('div[role=progressbar]', {hidden: true});
 
     //Click on the show button
+    console.log('pressing view...');
     await page.evaluate(() => {
-        document.querySelectorAll('div[role=button] span').forEach(function(elm, i) { 
-            if (elm.innerText == 'View') {
-                console.log('CLICK');
-                eventFire(elm, 'click');
-            }
-        
-            function eventFire(el, etype){
-            if (el.fireEvent) {
-                el.fireEvent('on' + etype);
-            } else {
-                var evObj = document.createEvent('Events');
-                evObj.initEvent(etype, true, false);
-                el.dispatchEvent(evObj);
-            }
-            }
-        });
+        const timeout = setInterval(() => {
+            document.querySelectorAll('div[role=button] span').forEach(function(elm, i) { 
+                if (elm.innerText == 'View') {
+                    console.log('CLICK');
+                    eventFire(elm, 'click');
+                }
+            
+                function eventFire(el, etype){
+                if (el.fireEvent) {
+                    el.fireEvent('on' + etype);
+                } else {
+                    var evObj = document.createEvent('Events');
+                    evObj.initEvent(etype, true, false);
+                    el.dispatchEvent(evObj);
+                }
+                }
+            });
+        }, 300);
     });
-    console.log('Point', index++);
     await page.waitForSelector('img[alt=Image]', {hidden: false});
     
     //Grab the images
-    console.log('Point', index++);
+    console.log('Evaluating shit...');
     images      = await page.$$eval("img[alt=Image]", imgs => imgs.map(img => img.getAttribute('src')));
-    console.log('Point', index++);
     tags        = await page.$$eval("a[href^=\"/hashtag\"]", tags => tags.map(tag => tag.innerText.substr(1) ));
-    console.log('Point', index++);
     articles    = await page.$$eval('article', articles => articles.map(art => art.innerText));
-    //Close
-    console.log('Point', index++);
+
+    //Close    
+    console.log('closing and returning...');
     await browser.close();
-    console.log('Point', index++);
+    
 
     //Finally return
     return {
